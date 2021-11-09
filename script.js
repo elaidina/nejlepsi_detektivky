e46c5f5d3a3aa203dc261e89830d1c1715db9c2e
Your Name <you@example.com>
2021-11-09 15:39:24 +0100

    m

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const toggles = document.querySelectorAll('.faq-toggle')
+const open = document.querySelector('#open')
+const close = document.querySelector('#close')
 
 toggles.forEach(toggle => {
     toggle.addEventListener('click', () => {
         toggle.parentNode.classList.toggle('active')
     })
 })
+
+
+open.addEventListener('click', () => {
+    toggles.forEach(toggle => {
+
+        
+    toggle.parentNode.classList.add('active') } 
+    
+    
+    )})
+
+    close.addEventListener('click', () => {
+        toggles.forEach(toggle => {
+    
+            
+        toggle.parentNode.classList.remove('active') } 
+        
+        
+        )})
